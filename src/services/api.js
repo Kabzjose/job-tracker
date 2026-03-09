f29140194e77a4a35122f01c312ff76eaf556e1d
Joseph Ndungu <josekabz00@gmail.com>
2026-03-09 18:40:58 +0300

    Deploying frontend

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api=axios.create({
-    baseURL:'http://127.0.0.1:5000/api',
+    baseURL:'https://job-tracker-backend-u6xr.onrender.com/api',
     headers:{
         'Content-Type':'application/json'
     }
